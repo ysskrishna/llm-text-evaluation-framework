2e5a3f59ab517269a58e075d8021e87c0a4d8f23
ysskrishna <sivasaikrishnassk@gmail.com>
2025-08-09 14:43:34 +0530

    Siva | implement basic multi page app setup

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import streamlit as st
 def main():
     st.set_page_config(page_title="LLM Text Evaluation Framework", layout="wide")
     
-    st.sidebar.header("LLM Text Evaluation Framework")
+
+    st.title("LLM Text Evaluation Framework")
+    st.write("This is the home page")
 
 
 if __name__ == "__main__":
---
+++ pages/1_history.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("History")
+st.write("This is the history page")
